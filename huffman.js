5f3dc40e0ddbd863cd9beaf1ee9480c0f9ee10aa
Alex Spitzer <spitzer.alex@gmail.com>
2018-08-11 17:20:14 -0400

    huffman now uses variable block size

--- huffman.js
+++ huffman.js
@@ -1,18 +1,36 @@
 //
 function myFunction() {
+	control = 0;
+	blah = parseInt(document.getElementById("blockSize").value);
+	huff(blah, false);
+}
+
+function huff(blockSize, repeat) {
 	// Read in word
 	var input = document.getElementById("myText").value.toLowerCase().replace(/\s/g, "_");
-	
+	if (input.length/blockSize < 2) {
+		document.getElementById("demo1").innerHTML = "<p>The characters/block must be " + Math.floor(input.length/2) + " or less for the given input.  Please try again.</p>";
+		for (y in divs) {
+			console.log(y);
+			document.getElementById(divs[y]).innerHTML = "<p></p>";
+		}
+		return;
+	} else {
+		document.getElementById("demo1").innerHTML = "";
+	}
 	// count characters for their frequency
 	var freq = {};
 	var index = 0;
-	for (var i = 0; i < input.length; i++) {
-    	if (input[i] in freq) {
-    		freq[input[i]]++;
+	
+	for (var i = 0; i < input.length; i+=blockSize) {
+    	var block = input.substring(i, i+blockSize);
+    	if (block in freq) {
+    		freq[block]++;
     	} else {
-    		freq[input[i]] = 1;
+    		freq[block] = 1;
     	}
 	}
+	
 	//document.getElementById("demo1").innerHTML = JSON.stringify(freq);
 	
 	/*testing = new Tree(1);
@@ -24,33 +42,41 @@ function myFunction() {
 	testing.print();*/
 	var queue = [];
 	var filler;
+	var blocks = [];
 	var numLetters = 0;
-	var letters = [];
 	for (z in freq) { 
     	filler = new Node(z + ", " + freq[z]);
-    	letters[numLetters] = z;
+    	blocks[numLetters] = z;
     	queue[numLetters] = filler;
     	numLetters++;
 	}
+
+	var letters = [];
+	for (var i = 0; i < input.length; i++) {
+    	var block = input.substring(i, i+1);
+    	if (letters.includes(block)) {
+    		
+    	} else {
+    		letters.push(block);
+    	}
+	}
 	var bin = [];
-	
 	for (var i = 0; i < letters.length; i++) {
 		if (bin[i] == null) {
 			bin[i] = i.toString(2)
 		}
-		if (bin[i].length < Math.ceil(Math.log2(numLetters))) {
+		if (bin[i].length < Math.ceil(Math.log2(letters.length))) {
 			bin[i] = "0" + bin[i];
 			i--;
 		}
 	}
 	scale(numLetters + 0);
-	control = 0;
-	printAll(queue, 0);
-	
+	printAll(queue, control);
 	queue.sort(function(a, b){return parseInt((a.hasOwnProperty("_root") ? a._root : a).data.substring((a.hasOwnProperty("_root") ? a._root : a).data.length - 2, (a.hasOwnProperty("_root") ? a._root : a).data.length)) - parseInt((b.hasOwnProperty("_root") ? b._root : b).data.substring((b.hasOwnProperty("_root") ? b._root : b).data.length - 2, (b.hasOwnProperty("_root") ? b._root : b).data.length))}).reverse();
-	control = 1;
-	printAll(queue, 1);
-	
+	if (!repeat) {
+		control++;
+		printAll(queue, control);
+	}
 	for (var i = 0; i < numLetters - 1; i++) {
 		// Sort the queue
 		queue.sort(function(a, b){return parseInt((a.hasOwnProperty("_root") ? a._root : a).data.substring((a.hasOwnProperty("_root") ? a._root : a).data.length - 2, (a.hasOwnProperty("_root") ? a._root : a).data.length)) - parseInt((b.hasOwnProperty("_root") ? b._root : b).data.substring((b.hasOwnProperty("_root") ? b._root : b).data.length - 2, (b.hasOwnProperty("_root") ? b._root : b).data.length))}).reverse();
@@ -71,14 +97,14 @@ function myFunction() {
 		tree.add(y, zData, tree.traverseDF);
 		queue.push(tree);
 		scale(queue.length)
-		if (i < 2) {
+		if (i < 2 && !repeat) {
 			control++;
 			printAll(queue, control);
 		}
 		/*control++;
 		printAll(queue, control);*/
 	}
-	control = 4;
+	control++;
 	printAll(queue, control);
 	
 	
@@ -86,20 +112,24 @@ function myFunction() {
 	complete.encode();
 	var someString = "";
 	var huffSize = [];
-	for (var i = 0; i < letters.length; i++) {
-		var code = complete.find(letters[i], complete.traverseDF);
-    	someString += letters[i] + ": " + code.encoded + "</br>";
+	for (var i = 0; i < blocks.length; i++) {
+		var code = complete.find(blocks[i], complete.traverseDF);
+    	someString += blocks[i] + ": " + code.encoded + "</br>";
+    	//console.log(blocks[i] + ": " + code.encoded);
     	huffSize[i] = code.encoded.length;
 	}
 	someString += "</br>So the string \"" + input + "\" becomes ";
 	var huffEncoded = "";
-	for (var i = 0; i < input.length; i++) {
-		var code = complete.find(input[i], complete.traverseDF);
+	for (var i = 0; i < input.length; i+=blockSize) {
+		var block = input.substring(i, i+blockSize);
+		//console.log(block.length);
+		var code = complete.find(block, complete.traverseDF);
+		//console.log(code);
     	someString += code.encoded;
     	huffEncoded += code.encoded;
 	}
-	control = 5;
-	document.getElementById(divs[control]).innerHTML = words[control] + "<p>" + someString + ".</p>";
+	control++;
+	document.getElementById(divs[control]).innerHTML = words[control] + "<p>" + someString + "</p>";
 	
 	someString = "";
 	for (var i = 0; i < letters.length; i++) {
@@ -109,22 +139,26 @@ function myFunction() {
 	someString += "</br>So the string \"" + input + "\" would have been ";
 	var binEncoded = "";
 	for (var i = 0; i < input.length; i++) {
-		var q = findIndex(letters, input[i]);
+		var q = findIndex(letters, input.substring(i, i+1));
 		var code = bin[q];
     	someString += code;
     	binEncoded += code;
 	}
-	control = 6;
-	document.getElementById(divs[control]).innerHTML = words[control] + Math.ceil(Math.log2(numLetters)) + " bits/character:</p>" + "<p>" + someString + ".</p>";
+	control++;
+	document.getElementById(divs[control]).innerHTML = words[control] + Math.ceil(Math.log2(letters.length)) + " bits/character:</p>" + "<p>" + someString + "</p>";
 	
-	control = 7;
+	control++;
 	var diff = binEncoded.length - huffEncoded.length;
 	var perc = Math.ceil(100*(diff/binEncoded.length));
 	var bpc = null;
-	for(var i = 0; i < letters.length; i++) {
-		bpc += huffSize[i]*freq[letters[i]];
+	for(var i = 0; i < blocks.length; i++) {
+		bpc += huffSize[i]*freq[blocks[i]];
 	}
-	bpc /= input.length;
+	console.log(bpc);
+	bpc /= Math.ceil(input.length/blockSize);
+	console.log(bpc);
+	bpc /= blockSize;
+	console.log(bpc);
 	bpc = Math.round(100*bpc)/100;
 	document.getElementById(divs[control]).innerHTML = "<p>The huffman code was " + diff + " characters smaller, meaning it used " + perc + "% less space with " + bpc + " bits/character</p>";
 }
@@ -148,15 +182,16 @@ function printAll(array, index) {
 		}
 	}
 	//console.log(maxI);
-	document.getElementById(divs[index]).innerHTML = words[control] + "<svg width ='1250' height =" + (yScale*(maxI+1)) + " fill = black>" + prnt + "</svg>";
+	document.getElementById(divs[control]).innerHTML = words[index] + "<svg width ='1250' height =" + (yScale*(maxI+1)) + " fill = black>" + prnt + "</svg>";
 }
 
 //var text1 = "";
 //var text2 = "";
+var blah;
 var xScale = 50;
 var yScale = 25;
 var divs = ["demo2", "demo3", "demo4", "demo5", "demo6", "demo7", "demo8", "demo9", "demo10", "demo11", "demo12", "demo13", "demo14", "demo15", "demo16", "demo17"];
-var words = ["<p>To start, Huffman codes determine the frequency of every character in the string, and makes nodes.  Here, each node is labeled with a character, and it's corresponding frequency.</p>", "<p>Then we order the nodes from highest-to-lowest frequency in a low priority queue.</p>", "<p>Take the 2 nodes with the smallest frequencies, and make them siblings in a tree.  The parent of these 2 nodes has a frequency equal to the sum of both node's frequencies, and replaces it's children in the queue.</p>", "<p>Repeat the previous 2 steps.</p>", "<p>Continue in this pattern until all the nodes are in 1 tree.</p>", "<p>Now using this tree, we can determine the binary values of each character.  Starting from the parent node, follow the branches until your at a leaf.  Keep track of every branch (0 is left, 1 is right) to find the binary representation of each character.</p>", "<p>Compare this to the encoding of ", ""];
+var words = ["<p>To start, Huffman codes determine the frequency of every character in the string, and makes nodes.  Here, each node is labeled with a character, and it's corresponding frequency.</p>", "<p>Then we order the nodes from highest-to-lowest frequency in a low priority queue.</p>", "<p>Take the 2 nodes with the smallest frequencies, and make them siblings in a tree.  The parent of these 2 nodes has a frequency equal to the sum of both node's frequencies, and replaces it's children in the queue.</p>", "<p>Repeat the previous 2 steps.</p>", "<p>Continue in this pattern until all the nodes are in 1 tree.</p>", "<p>Now using this tree, we can determine the binary values of each character.  Starting from the parent node, follow the branches until your at a leaf.  Keep track of every branch (0 is left, 1 is right) to find the binary representation of each character.</p>", "<p>Compare this to the encoding of ", "block size 2", "tree", "encoding", "bad encoding of "];
 var control = 0;
 
 Node.prototype.print = function(x, y) {
@@ -168,7 +203,7 @@ Node.prototype.print = function(x, y) {
 Tree.prototype.find = function(findData, traversal) {
     var blah = "42", 
     	callback = function(node) {
-            if (node.data.charAt(0) == findData && node.children.length == 0) {
+            if (node.data.substring(0, findData.length) == findData && node.children.length == 0) {
                 blah = node;
             }
         };
@@ -203,7 +238,7 @@ Tree.prototype.print = function(x, y) {
             	node.posY = node.parent.posY + yScale;
             	var foo = Math.pow(scl, (node.posY/25));
             	//console.log(foo);
-            	node.posX = node.parent.posX + 25*(j%2 ? -1 : 1)*Math.pow(scl, depth)/foo;
+            	node.posX = node.parent.posX + (20 + 4*(blah - 1))*(j%2 ? -1 : 1)*Math.pow(scl, depth)/foo;
             	
             	text1 += "<line x1=" + node.posX + " y1=" + node.posY + " x2=" + node.parent.posX + " y2=" + node.parent.posY + " style='stroke:rgb(200,200,200);stroke-width:2' />"
             }
