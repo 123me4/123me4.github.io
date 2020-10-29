c0e0280746c5849c1d7a4e5a569178f558a98488
Alex Spitzer <spitzer.alex@gmail.com>
2020-10-29 16:57:54 -0400

    encompass all in function

--- script.js
+++ script.js
@@ -1,67 +1,54 @@
-var img_ele = null,
-  x_cursor = 0,
-  y_cursor = 0,
-  x_img_ele = 0,
-  y_img_ele = 0;
-
-/* 
-function zoom(zoomincrement) {
-  img_ele = document.getElementById('drag-img');
-  var pre_width = img_ele.getBoundingClientRect().width, pre_height = img_ele.getBoundingClientRect().height;
-  img_ele.style.width = (pre_width * zoomincrement) + 'px';
-  img_ele.style.height = (pre_height * zoomincrement) + 'px';
-  img_ele = null;
-}
-
-document.getElementById('zoomout').addEventListener('click', function() {
-  zoom(0.5);
-});
-document.getElementById('zoomin').addEventListener('click', function() {
-  zoom(1.5);
-});
- */
-
-function start_drag() {
-  img_ele = this;
-  x_img_ele = window.event.clientX - document.getElementById('drag-img').offsetLeft;
-  y_img_ele = window.event.clientY - document.getElementById('drag-img').offsetTop;
-
-  console.log("start drag");
-}
-
-function stop_drag() {
-  img_ele = null;
-  
-  console.log("stop drag");
-}
-
-function while_drag() {
-  var x_cursor = window.event.clientX;
-  var y_cursor = window.event.clientY;
-  if (img_ele !== null) {
-    img_ele.style.left = (x_cursor - x_img_ele) + 'px';
-    img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
-
-      console.log(img_ele.style.left+' - '+img_ele.style.top);
-
+(function() {
+  function hasTouch() {
+    return 'ontouchstart' in document.documentElement;
   }
-}
+  var img_ele = null,
+    event_start = hasTouch() ? 'touchstart' : 'mousedown',
+    event_move = hasTouch() ? 'touchmove' : 'mousemove',
+    event_end = hasTouch() ? 'touchend' : 'mouseup';
+  console.log(event_start + "|" + event_move + "|" + event_end);
+  (function() {
+    function zoom(zoomincrement) {
+      img_ele = document.getElementById('drag-img');
+      var pre_width = img_ele.getBoundingClientRect().width,
+        pre_height = img_ele.getBoundingClientRect().height;
+      img_ele.style.width = (pre_width * zoomincrement) + 'px';
+      img_ele.style.height = (pre_height * zoomincrement) + 'px';
+      img_ele = null;
+    }
+    document.getElementById('zoomout').addEventListener('click', function() {
+      zoom(0.5);
+    });
+    document.getElementById('zoomin').addEventListener('click', function() {
+      zoom(1.5);
+    });
+  }());
+  (function() {
+    function start_drag(event) {
+      var x_cursor = hasTouch() ? event.changedTouches[0].clientX : event.clientX,
+        y_cursor = hasTouch() ? event.changedTouches[0].clientY : event.clientY;
+      img_ele = this;
+      x_img_ele = x_cursor - img_ele.offsetLeft;
+      y_img_ele = y_cursor - img_ele.offsetTop;
+      console.log("start drag");
+    }
 
-var di = document.getElementById('drag-img');
-if(di) {
-	di.addEventListener('mousedown', start_drag);
-	
-	console.log("image event added");
-} else {
-	console.log("image null");
-}
+    function stop_drag() {
+      img_ele = null;
+      console.log("stop drag");
+    }
 
-var con = document.getElementById('container')
-if(con) {
-	con.addEventListener('mousemove', while_drag);
-	con.addEventListener('mouseup', stop_drag);
-	
-	console.log("container events added");
-} else {
-	console.log("container null");
-}
+    function while_drag(event) {
+      var x_cursor = hasTouch() ? event.changedTouches[0].clientX : event.clientX,
+        y_cursor = hasTouch() ? event.changedTouches[0].clientY : event.clientY;
+      if (img_ele !== null) {
+        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
+        img_ele.style.top = (y_cursor - y_img_ele) + 'px';
+        console.log('dragging > img_left:' + img_ele.style.left + ' | img_top: ' + img_ele.style.top);
+      }
+    }
+    document.getElementById('drag-img').addEventListener(event_start, start_drag);
+    document.getElementById('container').addEventListener(event_move, while_drag);
+    document.getElementById('container').addEventListener(event_end, stop_drag);
+  }());
+}());
